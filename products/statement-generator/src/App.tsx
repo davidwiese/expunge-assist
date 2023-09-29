22891107bcfdda3b712b1fcea27aad7249081b74
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-09-29 11:52:59 -0700

    Remove trailing / from Router in App.tsx

--- products/statement-generator/src/App.tsx
+++ products/statement-generator/src/App.tsx
@@ -53,7 +53,7 @@ const App: React.FC = () => {
 
   return (
     <ThemeProvider theme={customMuiTheme}>
-      <Router basename={`${process.env.PUBLIC_URL}/`} hashType="slash">
+      <Router basename={`${process.env.PUBLIC_URL}`} hashType="slash">
         <RoutingContextProvider>
           <AffirmationContextProvider>
             <FormStateContextProvider>
