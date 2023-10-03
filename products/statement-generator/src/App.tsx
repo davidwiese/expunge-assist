28ad3dd93b17d78ba0561bf07cb3c57b4c4eb5d2
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-10-03 13:22:16 -0700

    Update Router basename to ./ as a test

--- products/statement-generator/src/App.tsx
+++ products/statement-generator/src/App.tsx
@@ -53,7 +53,7 @@ const App: React.FC = () => {
 
   return (
     <ThemeProvider theme={customMuiTheme}>
-      <Router basename="/expunge-assist">
+      <Router basename="./">
         <RoutingContextProvider>
           <AffirmationContextProvider>
             <FormStateContextProvider>
